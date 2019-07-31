ad71f1d8bd347d24a30df6c74034bd12414c793b
tanpham <tanpham@emotiv.com>
2019-07-31 11:39:52 +0700

    up to date

--- book/code/scrape_data_inside_table.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup(open('sample.html'), 'lxml')
-
-# print soup.prettify()
-
-for tr in soup.find_all('tr'):
-	for td in tr.find_all('td'):
-		print td.text
